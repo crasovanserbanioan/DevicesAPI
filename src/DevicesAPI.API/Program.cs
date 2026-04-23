afcc0a3b4944b5abad51fe36134ce9e9cbfbbe83
Serban Crasovan <crasovanserbanioan@gmail.com>
2026-04-23 20:01:20 +0300

    Populate program.cs: register services, add swagger, middleware, documentation, register components

--- src/DevicesAPI.API/Program.cs
+++ src/DevicesAPI.API/Program.cs
@@ -1,23 +1,62 @@
+using DevicesAPI.API.Middleware;
+using DevicesAPI.Application.Interfaces;
+using DevicesAPI.Application.Services;
+using DevicesAPI.Domain.Interfaces;
+using DevicesAPI.Infrastructure.Data;
+using DevicesAPI.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// EF Core + PostgreSQL
+builder.Services.AddDbContext<DevicesDbContext>(options =>
+	options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// DI registrations
+builder.Services.AddScoped<IDeviceRepository, DeviceRepository>();
+builder.Services.AddScoped<IDeviceService, DeviceService>();
+
+// Controllers
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Swagger / OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+	options.SwaggerDoc("v1", new()
+	{
+		Title = "Devices API",
+		Version = "v1",
+		Description = "REST API for managing device resources."
+	});
+
+	var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+	var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+	options.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Apply pending EF migrations on startup
+using (var scope = app.Services.CreateScope())
 {
-    app.MapOpenApi();
+	var db = scope.ServiceProvider.GetRequiredService<DevicesDbContext>();
+	db.Database.Migrate();
 }
 
+// Middleware
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+	options.SwaggerEndpoint("/swagger/v1/swagger.json", "Devices API v1");
+	options.RoutePrefix = "swagger";
+});
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
